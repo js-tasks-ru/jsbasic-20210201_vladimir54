a99fdff0380206ebc70a5d1445160bcad7f7b2f2
VladimirVertushkov <vladimir.vertushkov@yanex.ru>
2021-02-09 13:53:42 +0500

    isEmpty

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,12 +4,12 @@
  * @returns {Number}
  */
 function sumSalary(salaries) {
-   let sum = 0;
-
- 	for (let key in salaries) {
- 		if (typeof salaries[key] == 'number' && isFinite(salaries[key])){
-   			sum+=salaries[key];
- 		}
- 	}
- return sum;
+  let sum = 0;
+  
+	for (let key in salaries) {
+		if (typeof salaries[key] == 'number' && isFinite(salaries[key])){
+  			sum+=salaries[key];
+		}
+	}
+return sum;
 }
