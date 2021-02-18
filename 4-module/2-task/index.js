4ce7053a5bb356c868ca1646758a8faf452a29e5
VladimirVertushkov <vladimir.vertushkov@yanex.ru>
2021-02-18 23:46:17 +0500

    MakeDiagonalRed and Highlight

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,7 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  for (let i = 0; i < table.rows.length; i++) {
+      let row = table.rows[i];
+      let td = row.cells[i];
+      td.style.backgroundColor = 'red';
+    }
 }
