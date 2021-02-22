0309448bdd0c0165d6088243de9d24b50b365ee5
VladimirVertushkov <vladimir.vertushkov@yanex.ru>
2021-02-22 17:45:19 +0500

    hideSelf and toggleText

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,12 @@
 function toggleText() {
-  // ваш код...
+  let btn = document.querySelector(".toggle-text-button");
+  let text = document.querySelector("#text");
+
+  function setAtt() {
+    if (text.hasAttribute('hidden') == true){
+      text.hidden = false;
+    }else{text.hidden = true;}
+  }
+  btn.addEventListener( "click" , setAtt);
+
 }
